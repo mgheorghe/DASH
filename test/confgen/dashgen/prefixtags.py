4e74005156601c256fc2923b91245e632fb8c845
Chris Sommers <chrispsommers@gmail.com>
2022-08-08 17:48:43 -0700

    Python generator paradigm for dash_gen. variables.py reduced scale for experimenting.

--- test/confgen/dashgen/prefixtags.py
+++ test/confgen/dashgen/prefixtags.py
@@ -1,15 +1,15 @@
 import ipaddress
-from dashgen.variables import *
+import orjson
+import io
+import pprint
 
-
-def generate():
-    print('    ' + os.path.basename(__file__))
-    prefixtag = []
+from variables import *
+def items():
     for eni_index in range(1, ENI_COUNT+1):
         IP_L = IP_L_START + (eni_index - 1) * IP_STEP4
         r_vpc = eni_index + ENI_L2R_STEP
         IP_R = IP_R_START + (eni_index - 1) * IP_STEP4
-        prefixtag.append(
+        yield \
             {
                 "PREFIX-TAG:VPC:%d" % eni_index: {
                     "prefix-tag-id": "%d" % eni_index,
@@ -19,8 +19,7 @@ def generate():
                     ]
                 },
             }
-        )
-        prefixtag.append(
+        yield \
             {
                 "PREFIX-TAG:VPC:%d" % r_vpc: {
                     "prefix-tag-id": "%d" % r_vpc,
@@ -30,5 +29,21 @@ def generate():
                     ]
                 },
             }
-        )
-    return {"prefix-tags": prefixtag}
+        
+    return
+        
+def dictName():
+    return 'prefix-tags'
+
+def toDict(tag=dictName()):
+    return {dictName(): list(items())}
+
+def generate(tag=dictName()):
+    print('    ' + os.path.basename(__file__) + '.items()')
+    return toDict(tag)
+
+def pretty():
+    pprint.pprint(generate())
+
+if __name__ == "__main__":
+    pretty()
