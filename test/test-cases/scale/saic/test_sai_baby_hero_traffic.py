351dc4c332612035fe1f7c4a2b86fd14062d1104
Mircea Dan <mircea-dan.gheorghe@keysight.com>
2024-06-03 18:30:02 -0700

    Update test_sai_baby_hero_traffic.py

--- test/test-cases/scale/saic/test_sai_baby_hero_traffic.py
+++ test/test-cases/scale/saic/test_sai_baby_hero_traffic.py
@@ -12,6 +12,19 @@ import dpugen
 import json
 from pathlib import Path
 from pprint import pprint
+import ipaddress
+import socket
+import struct
+import macaddress
+import ipaddress
+from copy import deepcopy
+from munch import DefaultMunch
+# utils
+socket_inet_ntoa = socket.inet_ntoa
+struct_pack = struct.pack
+class MAC(macaddress.MAC):
+    formats = ('xx:xx:xx:xx:xx:xx',) + macaddress.MAC.formats
+maca = MAC       # optimization so the . does not get executed multiple times
 
 baby_hero_params = {                    # CONFIG VALUE             # DEFAULT VALUE
     'DPUS':                             1,                         # 8
@@ -63,61 +76,152 @@ class TestSaiBabyHero:
 
         conf = dpugen.sai.SaiConfig()
         conf.mergeParams(baby_hero_params)
-        DPU_VTEP_IP = conf.params_dict['LOOPBACK']
-        ENI_VTEP_IP = conf.params_dict['PAL']
 
-        ENI_IP = conf.params_dict['IP_L_START']
-        NETWORK_IP1 = conf.params_dict['IP_R_START']
+        dflt_params = {                        # CONFIG VALUE             # DEFAULT VALUE
+            'SCHEMA_VER':                      '0.0.4',
 
-        INNER_SRC_MAC = conf.params_dict['MAC_L_START']
-        INNER_DST_MAC = conf.params_dict['MAC_R_START']
+            'DC_START':                        '220.0.1.1',                # '220.0.1.2'
+            'DC_STEP':                         '0.0.1.0',                  # '0.0.1.0'
 
-        f1 = dataplane.configuration.flows.flow(name="ENI1_TO_NETWORK")[-1]
-        f1.tx_rx.port.tx_name = dataplane.configuration.ports[0].name
-        f1.tx_rx.port.rx_name = dataplane.configuration.ports[1].name
-        f1.size.fixed = 256
-        f1.duration.fixed_packets.packets = 1
-        f1.rate.pps = 10
-        f1.metrics.enable = True
+            'LOOPBACK':                        '221.0.0.1',                # '221.0.0.1'
+            'PAL':                             '221.1.0.0',                # '221.1.0.1'
+            'PAR':                             '221.2.0.0',                # '221.2.0.1'
+            'GATEWAY':                         '222.0.0.1',                # '222.0.0.1'
 
-        outer_eth1, ip1, udp1, vxlan1, inner_eth1, inner_ip1, inner_udp1= (
-                f1.packet.ethernet().ipv4().udp().vxlan().ethernet().ipv4().udp()
-        )
+            'DPUS':                             8,                         # 1
 
-        outer_eth1.src.value = '00:00:00:00:00:00'
-        outer_eth1.dst.value = '00:00:00:00:00:00'
-        outer_eth1.ether_type.value = 2048
+            'ENI_START':                        1,                         # 1
+            'ENI_COUNT':                        256,                       # 32
+            'ENI_STEP':                         1,                         # 1
+            'ENI_L2R_STEP':                     0,                      # 1000
 
-        print('221.1.0.0')
-        print(ENI_VTEP_IP)
-        ip1.src.value = '221.1.0.0' #ENI_VTEP_IP #ENI - VTEP
+            'VNET_PER_ENI':                     1,                         # 16 TODO: partialy implemented
 
-        print('221.0.0.1')
-        print(DPU_VTEP_IP)
-        ip1.dst.value = '221.0.0.1' #DPU_VTEP_IP #DPU - VTEP
+            'ACL_NSG_COUNT':                    5,                         # 5 (per direction per ENI)
+            'ACL_RULES_NSG':                    1000,                    # 1000
+            'IP_PER_ACL_RULE':                  1,                       # 100
+            'ACL_MAPPED_PER_NSG':               500,                     # 500, efective is 250 because denny are skiped
 
-        udp1.src_port.value = 11638
-        udp1.dst_port.value = 4789
+            'MAC_L_START':                      '00:1A:C5:00:00:01',
+            'MAC_R_START':                      '00:1B:6E:00:00:01',
 
-        vxlan1.vni.value = 1
-        vxlan1.reserved0.value = 0
-        vxlan1.reserved1.value = 0
+            'MAC_STEP_ENI':                     '00:00:00:18:00:00',       # '00:00:00:18:00:00'
+            'MAC_STEP_NSG':                     '00:00:00:02:00:00',
+            'MAC_STEP_ACL':                     '00:00:00:00:01:00',
 
-        inner_eth1.src.value = '00:1A:C5:00:00:01' #INNER_SRC_MAC
-        inner_eth1.dst.value = '00:1B:6E:00:00:01' #INNER_DST_MAC
+            'IP_L_START':                       '1.1.0.1',                 # local, eni
+            'IP_R_START':                       '1.4.0.1',                 # remote, the world
 
-        inner_ip1.src.value = '1.1.0.1' #ENI_IP   #ENI
-        inner_ip1.dst.value = '1.4.0.1' #NETWORK_IP1  #world
+            'IP_STEP1':                         '0.0.0.1',
+            'IP_STEP_ENI':                      '0.64.0.0',
+            'IP_STEP_NSG':                      '0.2.0.0',
+            'IP_STEP_ACL':                      '0.0.1.0',
+            'IP_STEPE':                         '0.0.0.2',
 
-        inner_udp1.src_port.value = 10000
-        inner_udp1.dst_port.value = 20000
+            'TOTAL_OUTBOUND_ROUTES':            25600000                  # ENI_COUNT * 100K
+        }
+
+        params_dict = deepcopy(dflt_params)
+
+        cooked_params_dict = {}
+        for ip in [
+            'IP_STEP1',
+            'IP_STEP_ENI',
+            'IP_STEP_NSG',
+            'IP_STEP_ACL',
+            'IP_STEPE',
+            'IP_L_START',
+            'IP_R_START',
+            'PAL',
+            'PAR',
+            'GATEWAY'
+        ]:
+            cooked_params_dict[ip] = int(ipaddress.ip_address((params_dict[ip])))
+        for mac in [
+            'MAC_L_START',
+            'MAC_R_START',
+            'MAC_STEP_ENI',
+            'MAC_STEP_NSG',
+            'MAC_STEP_ACL'
+        ]:
+            cooked_params_dict[mac] = int(maca(params_dict[mac]))
+
+        params = DefaultMunch.fromDict(params_dict)
+        cooked_params = DefaultMunch.fromDict(cooked_params_dict)
+
+        p = params
+        ip_int = cooked_params
+
+        flows = []
+
+        for eni_index, eni in enumerate(range(1, 33, 1)):
+
+            f1 = dataplane.configuration.flows.flow(name="ENI%d_TO_NETWORK" % eni)[-1]
+            f1.tx_rx.port.tx_name = dataplane.configuration.ports[0].name
+            f1.tx_rx.port.rx_name = dataplane.configuration.ports[1].name
+            f1.size.fixed = 256
+            f1.duration.fixed_packets.packets = 250
+            f1.rate.pps = 2 # unable to send more than 400 pps
+            f1.metrics.enable = True
+
+            outer_eth1, ip1, udp1, vxlan1, inner_eth1, inner_ip1, inner_udp1= (
+                    f1.packet.ethernet().ipv4().udp().vxlan().ethernet().ipv4().udp()
+            )
+
+            remote_ip_a_eni = ip_int.IP_R_START + eni_index * ip_int.IP_STEP_ENI
+            remote_mac_a_eni = ip_int.MAC_R_START + eni_index * ip_int.MAC_STEP_ENI
+
+            eni_ip = socket_inet_ntoa(struct_pack('>L', ip_int.IP_L_START + eni_index * ip_int.IP_STEP_ENI))
+            eni_mac = str(maca(ip_int.MAC_L_START + eni_index * ip_int.MAC_STEP_ENI))
+
+
+            underlay_ip_eni = socket_inet_ntoa(struct_pack('>L', ip_int.PAL + eni_index * ip_int.IP_STEP1))
+
+
+            outer_eth1.src.value = '00:00:00:00:00:00'
+            outer_eth1.dst.value = '00:00:00:00:00:00'
+            outer_eth1.ether_type.value = 2048
+
+
+            ip1.src.value = underlay_ip_eni #ENI_VTEP_IP #ENI - VTEP
+
+            ip1.dst.value = '221.0.0.1' #DPU_VTEP_IP #DPU - VTEP
+
+            udp1.src_port.value = 11638
+            udp1.dst_port.value = 4789
+
+            vxlan1.vni.value = eni
+            vxlan1.reserved0.value = 0
+            vxlan1.reserved1.value = 0
+
+            inner_eth1.src.value = eni_mac #INNER_SRC_MAC
+            #inner_eth1.dst.value = '00:1B:6E:00:00:01' #INNER_DST_MAC
+            inner_eth1.dst.increment.start = str(maca(remote_mac_a_eni))
+            inner_eth1.dst.increment.count = 250
+            inner_eth1.dst.increment.step  = '00:00:00:00:00:02'
+
+
+            inner_ip1.src.value = eni_ip #ENI_IP   #ENI
+
+            #import pdb; pdb.set_trace()
+            inner_ip1.dst.increment.start = socket_inet_ntoa(struct_pack('>L', remote_ip_a_eni)) #NETWORK_IP1  #world
+            inner_ip1.dst.increment.count = 250
+            inner_ip1.dst.increment.step  = '0.0.0.2'
+
+
+            inner_udp1.src_port.value = 10000
+            inner_udp1.dst_port.value = 20000
+
+            flows.append(f1)
 
         dataplane.set_config()
 
+        flow_names = [f.name for f in flows]    
+
         ts = dataplane.api.transmit_state()
         ts.state = ts.START
-        if f1.name != None:
-            ts.flow_names = [f1.name]
+        if len(flow_names) > 0:
+            ts.flow_names = flow_names
         dataplane.api.set_transmit_state(ts)
 
 
@@ -125,8 +229,6 @@ class TestSaiBabyHero:
         print('Expected\tTotal Tx\tTotal Rx')
         assert self.wait_for(lambda: self.metrics_ok(dataplane.api)), 'Metrics validation failed!'
 
-
-        flow_names=[f1.name]
 
         while(True):
             if (dataplane.is_traffic_stopped(flow_names)):
@@ -189,7 +291,7 @@ class TestSaiBabyHero:
 
         return expected == total_tx and total_rx >= expected
 
-    def wait_for(self, func, timeout=10, interval=0.2):
+    def wait_for(self, func, timeout=1000, interval=0.2):
         '''
         Keeps calling the `func` until it returns true or `timeout` occurs
         every `interval` seconds.
